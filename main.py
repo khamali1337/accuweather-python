ffd7caf0e91fb31916fac8be4c120f1d9f7e1da5
khamali1337 <khamali1337@yandex.ru>
2021-10-16 13:46:58 +0000

    update: all fixed

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'    
 }
 
-url1="https://www.accuweather.com/en/id/bayangkara/3490311/weather-forecast/3490311"
-url2="https://www.bmkg.go.id/cuaca/prakiraan-cuaca.bmkg?Kota=Jayapura&AreaID=501447&Prov=24"
+url1="https://www.accuweather.com/en/id/jayapura/205373/weather-forecast/205373"
+#url2="https://www.bmkg.go.id/cuaca/prakiraan-cuaca.bmkg?Kota=Jayapura&AreaID=501447&Prov=24"
 link=requests.get(url1,headers=header)
 #print(BeautifulSoup(link.content,'lxml'))
 sup=BeautifulSoup(link.content,'lxml')
 #tmp=sup.find_all('div',class_='kiri')
 #print(sup)
-
+location_=''
+temp_=''
 page=sup.find_all('div',class_='temp')
 for pg in page:
-       print(pg)
+       #print(pg)
+       l=pg.find('span',class_='after-temp').text
+       if l=='C':
+           temp_=pg.text
 
-#print('hello world')
-
+loc=sup.find('h1',class_='header-loc')
+location_=loc.text
+print()
+#--------------------
+print('AccuWeather Web Scrapping')
+print('----------------------------')
+print('Location   : '+location_)
+print('Temperature: '+temp_)
+print()
