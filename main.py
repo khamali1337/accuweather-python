c82c9416ff410aebb35bca9fa643f04b6840ea3f
khamali1337 <khamali1337@yandex.ru>
2021-10-16 11:25:55 +0000

    update: user agent fixed

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-#!/usr/bin/python3
+#!.env/bin/python3
 
-print('hello world')
+from bs4 import BeautifulSoup
+from lxml import html
+import requests,csv
+from urllib.parse import urlparse
+
+header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'    
+}
+
+url1="https://www.accuweather.com/en/id/bayangkara/3490311/weather-forecast/3490311"
+url2="https://www.bmkg.go.id/cuaca/prakiraan-cuaca.bmkg?Kota=Jayapura&AreaID=501447&Prov=24"
+link=requests.get(url1,headers=header)
+print(BeautifulSoup(link.content,'lxml'))
+#sup=BeautifulSoup(link,'html.parser')
+#tmp=sup.find_all('div',class_='kiri')
+#print(sup)
+
+#page=sup.find_all('p',class_='title')
+#for pg in tmp:
+#       print(pg)
+
+#print('hello world')
+
